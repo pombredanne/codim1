Worked on the inaccurate results some. Problem boils down to bad numerical integration methods. Rewriting the wrong matrix entries gives a good result. Going to attempt to implement Sauter-Schwab quadrature rules.

source/dof_handler.py
@@ -1,26 +1,117 @@
 import numpy as np
 
-class DOFHandler(object):
+class DiscontinuousDOFHandler(object):
     """
     Maps the degrees of freedom from an element local perspective
     to a global index. Allows assembly of global matrix operators.
+    The dofs on the boundary here have no support on the neighboring
+    element.
+
+    mesh is not used here, but added to the constructor to conform to
+    the interface
     """
-    def __init__(self, dim, n_elements, element_deg):
-        self.dim = dim
-        self.n_elements = n_elements
+    def __init__(self, mesh, element_deg):
+        self.dim = 2
         self.element_deg = element_deg
+        self.mesh = mesh
+
         # element_deg + 1 degrees of freedom per element.
         # A 1st order polynomial (ax + b) has 2 coefficients
         # also one dof per dimension
-        self.total_dofs = dim * n_elements * (element_deg + 1)
+        self.total_dofs = self.dim * self.mesh.n_elements * (element_deg + 1)
 
         # Making a dof_map in 1D is super simple! Its just a folded over list of all
         # the dofs
         self.dof_map = np.arange(self.total_dofs)\
-                       .reshape(dim, n_elements, element_deg + 1)
+                       .reshape(self.dim, self.mesh.n_elements, element_deg + 1)
 
-def test_dof_handler():
-    dh = DOFHandler(2, 2, 2)
+class ContinuousDOFHandler(object):
+    """
+    As opposed to DiscontinuousDOFHandler, the ContinuousDOFHandler makes sure that
+    adjacent elements share DOFs.
+    """
+    def __init__(self, mesh, element_deg):
+        self.dim = 2
+        if element_deg < 1:
+            raise Exception("Continuous element degree must be at least 1.")
+
+        self.element_deg = element_deg
+        self.mesh = mesh
+
+        # Loop over elements and attach local dofs to the global dofs vector
+
+        self.dof_map = np.empty((2, self.mesh.n_elements, self.element_deg + 1),
+                                dtype = np.int)
+        self.total_dofs = 0
+        d = 0
+        self.element_processed = [False] * mesh.n_elements
+        for k in range(mesh.n_elements):
+            # Handle left boundary
+            nghbr_left = self.mesh.neighbors[k][0]
+            if nghbr_left == -1 or not self.element_processed[nghbr_left]:
+                # Far left dof.
+                self.dof_map[d, k, 0] = self.total_dofs
+                self.total_dofs += 1
+            else:
+                self.dof_map[d, k, 0] = self.dof_map[d, nghbr_left, -1]
+
+            # Handle internal dofs.
+            for i in range(1, self.element_deg):
+                self.dof_map[d, k, i] = self.total_dofs
+                self.total_dofs += 1
+
+            # Handle right boundary
+            nghbr_right = self.mesh.neighbors[k][1]
+            if nghbr_right == -1 or not self.element_processed[nghbr_right]:
+                # Far right dof.
+                self.dof_map[d, k, -1] = self.total_dofs
+                self.total_dofs += 1
+            else:
+                self.dof_map[d, k, -1] = self.dof_map[d, nghbr_right, 0]
+
+            self.element_processed[k] = True
+
+        self.dof_map[1, :, :] = self.dof_map[0, :, :] + self.total_dofs
+
+        self.total_dofs *= 2
+
+
+################################################################################
+# TESTS                                                                        #
+################################################################################
+import mesh
+
+def test_discontinuous_dof_handler():
+    msh = mesh.Mesh.simple_line_mesh(2)
+    dh = DiscontinuousDOFHandler(msh, 2)
     assert(dh.dof_map[0, 0, 0] == 0)
     assert(dh.dof_map[0, 1, 1] == 4)
     assert(dh.dof_map[1, 0, 0] == 6)
+
+def test_continuous_dof_handler_total():
+    msh = mesh.Mesh.simple_line_mesh(4)
+    dh = ContinuousDOFHandler(msh, 1)
+    assert(dh.total_dofs == 10)
+
+def test_continuous_dof_handler_linear():
+    msh = mesh.Mesh.simple_line_mesh(4)
+    dh = ContinuousDOFHandler(msh, 1)
+    assert(dh.dof_map[0, 0, 1] == 1)
+    assert(dh.dof_map[0, 1, 0] == 1)
+    assert(dh.dof_map[1, 2, 1] == 8)
+    assert(dh.dof_map[1, 3, 0] == 8)
+
+def test_continuous_dof_handler_quadratic():
+    msh = mesh.Mesh.simple_line_mesh(2)
+    dh = ContinuousDOFHandler(msh, 2)
+    assert(dh.dof_map[0, 0, 2] == 2)
+    assert(dh.dof_map[0, 1, 0] == 2)
+    assert(dh.dof_map[0, 1, 1] == 3)
+
+def test_continuous_dof_handler_loop():
+    vertices = np.array([[0, 1], [1, 0]])
+    element_to_vertex = np.array([[0, 1], [1, 0]])
+    msh = mesh.Mesh(vertices, element_to_vertex)
+    dh = ContinuousDOFHandler(msh, 1)
+    assert(dh.dof_map[0, 0, 0] == dh.dof_map[0, 1, 1])
+    assert(dh.dof_map[0, 0, 1] == dh.dof_map[0, 1, 0])

source/elastic_kernel.py
@@ -3,14 +3,13 @@ import numpy as np
 class ElastostaticKernel(object):
     """
     A class to contain the elastostatic kernel calculations.
-
-    The kernels copied in here are almost certainly wrong. I need to go over
-    them carefully and compare to a trusted source. The printed versions seem
-    to all get the answer slightly wrong. Maybe look in Timoshenko...
     """
     def __init__(self, shear_modulus, poisson_ratio):
         self.shear_modulus = shear_modulus
         self.poisson_ratio = poisson_ratio
+
+        self.const1 = (1 - 2 * poisson_ratio);
+        self.const2 = 1 / (4 * np.pi * (1 - poisson_ratio))
 
     def displacement_singular(self, r, n):
         """The singular (log(r)) part of the displacement kernel"""
@@ -18,8 +17,7 @@ class ElastostaticKernel(object):
         pr = self.poisson_ratio
         dx = r[0]
         dy = r[1]
-        dist_squared = dx ** 2 + dy ** 2
-        dist = np.sqrt(dist_squared)
+        dist = np.sqrt(dx ** 2 + dy ** 2)
 
         outer_factor = 1.0 / (8.0 * np.pi * mu * (1 - pr))
 
@@ -44,6 +42,7 @@ class ElastostaticKernel(object):
             # scream if you divide by zero. So, we just return zero.
             return np.ones((2, 2))
         dist = np.sqrt(dist_squared)
+        dr = np.array([dx / dist, dy / dist])
 
         outer_factor = 1.0 / (8.0 * np.pi * mu * (1 - pr))
 
@@ -83,39 +82,63 @@ class ElastostaticKernel(object):
         ny = n[1]
         assert(nx ** 2 + ny ** 2 == 1.0)
 
-        dist_squared = dx ** 2 + dy ** 2
-        dist = np.sqrt(dist_squared)
+        dist = np.sqrt(dx ** 2 + dy ** 2)
         drdn = r.dot(n) / dist
-        outer_factor = -1.0 / (4.0 * np.pi * (1 - pr) * dist)
-
+        dr = np.array([dx / dist, dy / dist])
 
         T = np.empty((2, 2))
-        T[0, 0] = ((1 - 2 * pr) + 2 * (dx ** 2) / dist_squared) * drdn
-        T[1, 1] = ((1 - 2 * pr) + 2 * (dy ** 2) / dist_squared) * drdn
-        T[0, 1] = ((2 * dx * dy) / dist_squared) * drdn - \
-                  (1 - 2 * pr) * ((dx / dist) * ny - (dy / dist) * nx)
-        T[1, 0] = ((2 * dx * dy) / dist_squared) * drdn - \
-                  (1 - 2 * pr) * ((dy / dist) * nx - (dx / dist) * ny)
-        T *= outer_factor
+        T[0, 0] = self.traction_kernel_element(0, 0, dist, dr, drdn, n)
+        T[1, 1] = self.traction_kernel_element(1, 1, dist, dr, drdn, n)
+        T[0, 1] = self.traction_kernel_element(0, 1, dist, dr, drdn, n)
+        T[1, 0] = -T[0, 1]
         return T
+
+    def traction_kernel_element(self, i, j, dist, dr, drdn, normal):
+        return 1 / dist * self.const2 *\
+                (-drdn * (self.const1 * (i == j) +  2 * (dr[i] * dr[j])) + \
+                 self.const1 * (dr[i] * normal[j] - dr[j] * normal[i]))
+
+    # def traction_kernel_element(self, i, j, r, dr):
+    #     return const1*( dr(i)*dr(j) + (i==j) * ( const2*log(1/r) ) );
+
 
 ################################################################################
 # TESTS                                                                        #
 ################################################################################
 
+def test_traction_kernel_elements():
+    E = 1e5
+    nu = 0.3
+    shear_modulus = E / (2 * (1 + nu))
+    kernel = ElastostaticKernel(shear_modulus, nu)
+    r = 4.7285
+    dr = np.array([0, 1])
+    drdn = 0
+    normal = np.array([-1, 0])
+    T = np.zeros((2,2))
+    for i in range(2):
+        for j in range(2):
+           T[i, j] = kernel.traction_kernel_element(i, j, r, dr, drdn, normal)
+    exact = np.array([[0, 0.0096],[-0.0096, 0]])
+    np.testing.assert_almost_equal(exact, T, 4)
 
 def test_displacement_nonsingular_for_r_equal_to_0():
     kernel = ElastostaticKernel(30e9, 0.25)
     u_nonsing = kernel.displacement_nonsingular(np.array([0.0, 0.0]), 0.0)
     assert(not np.isnan(np.sum(u_nonsing)))
 
-def test_displacement_symmetric():
+def test_displacement_symmetry():
     kernel = ElastostaticKernel(1.0, 0.25)
     a = kernel.displacement_kernel([1.0, 0.5], 0.0)
-    b = kernel.displacement_kernel([-1.0, -0.5], 0.0)
+    np.testing.assert_almost_equal(a - a.T, np.zeros_like(a))
+
+def test_displacement_radial_symmetry():
+    kernel = ElastostaticKernel(1.0, 0.25)
+    a = kernel.displacement_kernel(np.array([1.0, 0.5]), 0.0)
+    b = kernel.displacement_kernel(np.array([-1.0, -0.5]), 0.0)
     np.testing.assert_almost_equal(a, b)
 
-def test_traction_symmetric():
+def test_traction_radial_symmety():
     kernel = ElastostaticKernel(1.0, 0.25)
     a = kernel.traction_kernel(np.array([1.0, 0.5]), np.array([1.0, 0.0]))
     # Only symmetric if we reverse the normal vector too!

source/main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from dof_handler import DOFHandler
+from dof_handler import DiscontinuousDOFHandler, ContinuousDOFHandler
 from basis_funcs import BasisFunctions
 from mesh import Mesh
 from assembler import Assembler
@@ -8,8 +8,11 @@ from elastic_kernel import ElastostaticKernel
 import tools
 
 def main():
+    # Should we plot
+    plot = True
+
     # Number of elements
-    n_elements = 40
+    n_elements = 2
 
     # Degree of the polynomial basis to use. For example, 1 is a linear basis
     element_deg = 1
@@ -22,49 +25,46 @@ def main():
     poisson_ratio = 0.25
 
     # Quadrature points for the various circumstances
-    quad_points_nonsingular = 5
+    quad_points_nonsingular = 15
     quad_points_logr = 5
-    quad_points_oneoverr = 5
+    quad_points_oneoverr = 15
 
 
     bf = BasisFunctions.from_degree(element_deg)
-    mesh = Mesh.simple_line_mesh(n_elements)
+    mesh = Mesh.simple_line_mesh(n_elements, 0.0, 1.0)
     kernel = ElastostaticKernel(shear_modulus, poisson_ratio)
-    dh = DOFHandler(2, n_elements, element_deg)
+    dh = DiscontinuousDOFHandler(mesh, element_deg)
     assembler = Assembler(mesh, bf, kernel, dh,
                           quad_points_nonsingular,
                           quad_points_logr,
                           quad_points_oneoverr)
-    G, H = assembler.assemble()
-    # tools.plot_matrix(G, 'G', show = False)
-    # tools.plot_matrix(H, 'H')
-
-
-    # Specify which boundary conditions we have for each element
-    # Traction for every element with t_x = 0, t_y = 1.0
-    fnc = lambda x: (0.0, 1.0)
-    tractions = tools.interpolate(fnc, dh, bf, mesh)
-    # displacements = tools.interpolate(fnc, dh, bf, mesh)
-    # bc_list = [('traction', fnc) for i in range(n_elements)]
-
-    np.set_printoptions(precision = 3)
+    H, G = assembler.assemble()
     import ipdb; ipdb.set_trace()
-    rhs = np.dot(H, tractions)
-    # rhs = np.dot(G, displacements)
+    # import ipdb; ipdb.set_trace()
+    # Setting the small values to zero just makes debugging easier
+    H[np.abs(H) < 1e-14] = 0.0
+    G[np.abs(G) < 1e-14] = 0.0
+    # tools.plot_matrix(H, 'H', show = False)
+    # tools.plot_matrix(G, 'G')
+
+
+    fnc = lambda x: (0.0, 1.0)
+    # Solve a dirichlet problem (specify displacement, derive traction)
+    displacements = tools.interpolate(fnc, dh, bf, mesh)
+    rhs = np.dot(H, displacements)
     soln = np.linalg.solve(G, rhs)
-    # soln = np.linalg.solve(H, rhs)
+    x, s = tools.evaluate_boundary_solution(5, soln, dh, bf, mesh)
 
-    x, u = tools.evaluate_boundary_solution(5, soln, dh, bf, mesh)
-
-    plt.figure()
-    plt.plot(x[:, 0], u[:, 0])
-    plt.xlabel(r'X')
-    plt.ylabel(r'$u_x$', fontsize = 18)
-    plt.figure()
-    plt.plot(x[:, 0], u[:, 1])
-    plt.xlabel(r'X')
-    plt.ylabel(r'$u_y$', fontsize = 18)
-    plt.show()
+    if plot:
+        plt.figure()
+        plt.plot(x[:, 0], s[:, 0])
+        plt.xlabel(r'X')
+        plt.ylabel(r'$t_x$', fontsize = 18)
+        plt.figure()
+        plt.plot(x[:, 0], s[:, 1])
+        plt.xlabel(r'X')
+        plt.ylabel(r'$t_y$', fontsize = 18)
+        plt.show()
 
 
     # See section 2.7 of starfield and crouch for the standard formulas to convert

source/mesh.py
@@ -22,16 +22,17 @@ class Mesh(object):
         self.n_elements = element_to_vertex.shape[0]
 
         self.compute_normals()
+        self.compute_connectivity()
 
     @classmethod
-    def simple_line_mesh(cls, n_elements):
+    def simple_line_mesh(cls, n_elements, left_edge = -1.0, right_edge = 1.0):
         """
         Create a mesh consisting of a line of elements starting at -1 and
         extending to +1 in x coordinate, y = 0.
         """
         n_vertices = n_elements + 1
         vertices = np.zeros((n_vertices, 2))
-        x_vals = np.linspace(-1.0, 1.0, n_vertices)
+        x_vals = np.linspace(left_edge, right_edge, n_vertices)
         vertices[:, 0] = x_vals
 
         element_to_vertex = np.zeros((n_elements, 2))
@@ -47,6 +48,26 @@ class Mesh(object):
         r = (all_vertices[:,1,:] - all_vertices[:,0,:])
         r_norm = np.linalg.norm(r, axis = 1)
         self.normals = np.vstack((-r[:, 1] / r_norm, r[:, 0] / r_norm)).T
+
+    def compute_connectivity(self):
+        """
+        Determine a store a representation of which elements are adjacent.
+        Simple for 2D.
+        """
+        # Create of list of which elements touch each vertex
+        # -1 if no element touches there
+        touch_vertex = -np.ones((self.n_vertices, 2))
+        for k in range(0, self.n_elements):
+            # The left vertex of an element has that element
+            # as its right neighbor and vice-versa
+            touch_vertex[self.element_to_vertex[k][0]][1] = k
+            touch_vertex[self.element_to_vertex[k][1]][0] = k
+
+        self.neighbors = np.zeros((self.n_elements, 2))
+        for k in range(0, self.n_elements):
+            self.neighbors[k][0] = touch_vertex[self.element_to_vertex[k][0]][0]
+            self.neighbors[k][1] = touch_vertex[self.element_to_vertex[k][1]][1]
+        self.neighbors = self.neighbors.astype(np.int)
 
     def get_physical_points(self, element_id, reference_pts):
         """
@@ -93,6 +114,11 @@ def test_simple_line_mesh():
     assert((m.element_to_vertex == correct_etov).all())
     assert(m.element_to_vertex.dtype.type is np.int64)
 
+def test_simple_line_mesh():
+    m = Mesh.simple_line_mesh(2, 3.0, 4.0)
+    correct_vertices = np.array([[3.0, 0.0], [3.5, 0.0], [4.0, 0.0]])
+    assert((m.vertices == correct_vertices).all())
+
 def test_get_phys_pts():
     m = Mesh.simple_line_mesh(4)
 
@@ -119,5 +145,22 @@ def test_normals():
     assert(m.normals.shape[0] == 4)
     assert((m.normals[:, 1] == [1, 1, 1, 1]).all())
 
-# Do some more tests with a more complex mesh.
+def test_connectivity():
+    m = Mesh.simple_line_mesh(4)
+    assert(m.neighbors.dtype == np.int)
+    assert(m.neighbors[0][0] == -1)
+    assert(m.neighbors[3][1] == -1)
 
+    assert(m.neighbors[2][0] == 1)
+    assert(m.neighbors[2][1] == 3)
+
+def test_connectivity_loop():
+    vertices = np.array([[0, 1], [1, 0]])
+    element_to_vertex = np.array([[0, 1], [1, 0]])
+    m = Mesh(vertices, element_to_vertex)
+
+    assert(m.neighbors.dtype == np.int)
+    assert(m.neighbors[0][0] == 1)
+    assert(m.neighbors[0][1] == 1)
+    assert(m.neighbors[1][0] == 0)
+    assert(m.neighbors[1][1] == 0)

source/tools.py
@@ -27,7 +27,7 @@ def interpolate(fnc, dof_handler, basis_funcs, mesh):
     basis function to be the value of fnc at the node (where phi(x) = 1).
     """
     result = np.empty(dof_handler.total_dofs)
-    for k in range(dof_handler.n_elements):
+    for k in range(mesh.n_elements):
         for i in range(basis_funcs.num_fncs):
             dof_x = dof_handler.dof_map[0, k, i]
             dof_y = dof_handler.dof_map[1, k, i]
@@ -51,7 +51,7 @@ def evaluate_boundary_solution(points_per_element, soln,
     """
     x = []
     y = []
-    for k in range(dof_handler.n_elements):
+    for k in range(mesh.n_elements):
         for pt in np.linspace(0.0, 1.0, points_per_element):
             x.append(mesh.get_physical_points(k, pt))
             ux = 0
@@ -79,7 +79,7 @@ def evaluate_solution_on_element(element_idx, reference_point, soln,
 ################################################################################
 # TESTS                                                                        #
 ################################################################################
-from dof_handler import DOFHandler
+from dof_handler import DiscontinuousDOFHandler
 from basis_funcs import BasisFunctions
 from mesh import Mesh
 
@@ -88,7 +88,7 @@ def test_interpolate():
     element_deg = 0
     bf = BasisFunctions.from_degree(element_deg)
     msh = Mesh.simple_line_mesh(n_elements)
-    dh = DOFHandler(2, n_elements, element_deg)
+    dh = DiscontinuousDOFHandler(msh, element_deg)
     fnc = lambda x: (x[0], x[1])
     val = interpolate(fnc, dh, bf, msh)
 
@@ -103,7 +103,7 @@ def test_evaluate_boundary_solution_easy():
     element_deg = 0
     bf = BasisFunctions.from_degree(element_deg)
     msh = Mesh.simple_line_mesh(n_elements)
-    dh = DOFHandler(2, n_elements, element_deg)
+    dh = DiscontinuousDOFHandler(msh, element_deg)
     fnc = lambda x: (x[0], x[1])
     solution = interpolate(fnc, dh, bf, msh)
     x, soln = evaluate_boundary_solution(11, solution, dh, bf, msh)
@@ -117,7 +117,7 @@ def test_evaluate_solution_on_element():
     element_deg = 1
     bf = BasisFunctions.from_degree(element_deg)
     msh = Mesh.simple_line_mesh(n_elements)
-    dh = DOFHandler(2, n_elements, element_deg)
+    dh = DiscontinuousDOFHandler(msh, element_deg)
     fnc = lambda x: (x[0], x[1])
     solution = interpolate(fnc, dh, bf, msh)
     eval = evaluate_solution_on_element(1, 1.0, solution,
@@ -132,7 +132,7 @@ def test_interpolate_evaluate_hard():
     element_deg = 6
     bf = BasisFunctions.from_degree(element_deg)
     msh = Mesh.simple_line_mesh(n_elements)
-    dh = DOFHandler(2, n_elements, element_deg)
+    dh = DiscontinuousDOFHandler(msh, element_deg)
     fnc = lambda x: (x[0] ** 6, 0)
     solution = interpolate(fnc, dh, bf, msh)
     x, soln = evaluate_boundary_solution(5, solution, dh, bf, msh)