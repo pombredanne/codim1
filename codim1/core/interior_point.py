New interior computation tools give a slightly (very?) wrong answer, with the correct shape. Unsettling.

codim1/core/interior_point.py
@@ -1,13 +1,10 @@
 import numpy as np
+from codim1.core.basis_funcs import Function
 from codim1.fast.integration import single_integral
 
 class InteriorPoint(object):
     """
-    Compute the value of the solution at an interior point.
-
-    A lot of this code is exactly what's found in the Assembler class. Seems
-    like an opportunity for some better abstraction.
-
+    Computes the value of the solution at an interior point.
     """
     #TODO: There are no tests for any of this. HOW TO TEST IT?
     def __init__(self,
@@ -24,80 +21,16 @@ class InteriorPoint(object):
         kernel must be a standard kernel function.
         solution must behave like a set of basis functions.
         """
-        G_local = self.interior_integral(
         result = np.zeros(2)
+
+        kernel_fnc = lambda x, n: kernel.call(x - pt, pt_normal, n)
+        one = Function(lambda x: np.ones(2))
         for k in range(self.mesh.n_elements):
-            for i in range(self.basis_funcs.num_fncs):
-                dofs = [self.dof_handler.dof_map[0, k, i],
-                        self.dof_handler.dof_map[1, k, i]]
-
-                kernel_fnc = lambda x, n: kernel.call(x - pt, pt_normal, n)
-
-                integral =
-
-                G_local = self.interior_integral(self.Guu,
-                                np.zeros((2, 2)),
-                                point, k, i)
-
-                H_local = self.interior_integral(self.Gup,
-                                np.zeros((2, 2)),
-                                point, k, i)
-
-                for a in range(2):
-                    for c in range(2):
-                        result[a] -= disp[dofs[c]] * H_local[a, c]
-                        result[a] += trac[dofs[c]] * G_local[a, c]
-
-        return result
-
-    def compute_stress(self, point, disp, trac):
-        stress = np.zeros((2, 2))
-
-        for k in range(self.mesh.n_elements):
-            for i in range(self.basis_funcs.num_fncs):
-                dofs = [self.dof_handler.dof_map[0, k, i],
-                        self.dof_handler.dof_map[1, k, i]]
-
-                Dx_local = self.interior_integral(self.Dijk,
-                                np.zeros((2, 2, 2)),
-                                point, k, i)
-
-                Sx_local = self.interior_integral(self.Sijk,
-                                np.zeros((2, 2, 2)),
-                                point, k, i)
-
-                for a in range(2):
-                    for b in range(2):
-                        for c in range(2):
-                            stress[a, b] -= disp[dofs[c]] * S_local[a, b, c]
-                            stress[a, b] += trac[dofs[c]] * D_local[a, b, c]
-
-        return stress
-
-    # TODO: Combine this single integral function with the one in
-    # the integration module.
-    def interior_integral(self, kernel, result, pt, k, i):
-        """
-        Performs an integral over a surface element, computing the solution
-        at an interior point. Basically, this is a collocation integral
-        originating from a point force (1, 1) at the interior point
-        """
-        jacobian = self.mesh.get_element_jacobian(k)
-        normal = self.mesh.normals[k]
-        q_pts = self.quadrature.x
-        w = self.quadrature.w
-        # Just perform standard gauss quadrature
-        for (q_pt, w) in zip(q_pts, w):
-            # The kernel is evaluated in physical coordinates
-            phys_soln_pt = self.mesh.get_physical_points(k, q_pt)
-
-            # The basis functions should be evaluated on reference
-            # coordinates
-            soln_basis_fnc = self.basis_funcs.evaluate_basis(i,
-                                    q_pt, phys_soln_pt)
-
-            r = phys_soln_pt - pt
-            result += kernel.call(r, np.zeros(2), normal) * \
-                soln_basis_fnc * jacobian * w
-
+            for i in range(solution.num_fncs):
+                integral = single_integral(self.mesh, kernel_fnc,
+                                           one, solution,
+                                           self.quadrature,
+                                           k, i, 0)
+                result[0] += integral[0, 0] + integral[0, 1]
+                result[1] += integral[1, 0] + integral[1, 1]
         return result

codim1/core/tools.py
@@ -21,8 +21,10 @@ import matplotlib.collections
 #     plt.colorbar()
 #     plt.show()
 
-def rmse(f1, f2):
-    return np.sqrt((np.sum(((f1 - f2) / (f2)) ** 2) / f2.shape[0]))
+def L2_error(f1, f2):
+    L2_f2 = np.sqrt(np.sum((f2 ** 2)))
+    L2_f1_minus_f2 = np.sqrt(np.sum(((f1 - f2) ** 2)))
+    return L2_f1_minus_f2 / L2_f2
 
 def plot_mesh(msh, show = True):
     points1 = msh.vertices[msh.element_to_vertex[:, 0]]

codim1/drivers/disk_compression.py
@@ -14,7 +14,7 @@ from codim1.fast.elastic_kernel import DisplacementKernel, TractionKernel, \
 from codim1.core.quad_strategy import QuadStrategy
 from codim1.core.quadrature import QuadGauss
 from codim1.core.mass_matrix import MassMatrix
-# from codim1.core.interior_point import InteriorPoint
+from codim1.core.interior_point import InteriorPoint
 import codim1.core.tools as tools
 
 def full_surface_traction(x, n):
@@ -41,17 +41,24 @@ def main(n_elements, element_deg, plot):
     # than necessary, but nice for testing other components
     interior_quad_pts = 13
 
-    # Is this an interior or exterior mesh?
+    # A circle with radius one.
+    # TODO: Implement isoparametric elements, so that curved surfaces can be
+    # better approximated.
     mesh = Mesh.circular_mesh(n_elements, 1.0)
 
-    #TODO: Implement isoparametric elements
     # Define the basis functions on the mesh.
     bf = BasisFunctions.from_degree(element_deg, mesh)
 
     # This object defines what type of quadrature to use for different
     # situations (log(r) singular, 1/r singular, adjacent elements, others)
     # and how many points to use.
-    qs = QuadStrategy(mesh, quad_min, quad_max, quad_logr, quad_oneoverr)
+    qs = QuadStrategy(mesh, quad_max, quad_max, quad_logr, quad_oneoverr)
+    # Use more point for the RHS. This is because it is discontinuous
+    # at: theta = (24/50) * pi, so if there aren't enough points, we will
+    # miss that discontinuity
+    # This is not necessary if the mesh is aligned with the discontinuity
+    qs_rhs = qs#QuadStrategy(mesh, quad_max, quad_max,
+                                 #quad_logr, quad_oneoverr)
 
     # The first two are elastic kernels for the displacement BIE,
     # these will be used to solve the BIE
@@ -59,13 +66,14 @@ def main(n_elements, element_deg, plot):
     # these will be used to compute interior stresses
     k_d = DisplacementKernel(shear_modulus, poisson_ratio)
     k_t = TractionKernel(shear_modulus, poisson_ratio)
-    k_ta = AdjointTractionKernel(1.0, 0.25)
-    k_h = HypersingularKernel(1.0, 0.25)
+    k_ta = AdjointTractionKernel(shear_modulus, poisson_ratio)
+    k_h = HypersingularKernel(shear_modulus, poisson_ratio)
 
-    # For a problem where the unknowns are displacements, we use
-    # a discontinuous basis.
+    # For a problem where the unknowns are displacements, use
+    # a discontinuous basis. If the unknowns were tractions, a
+    # continuous basis is used. These are constraints that follow from
+    # some functional analysis fiddle-diddle
     dh = DiscontinuousDOFHandler(mesh, element_deg)
-
 
     # Assemble the matrix of displacements induced by displacements at
     # another location.
@@ -86,11 +94,12 @@ def main(n_elements, element_deg, plot):
     # Make the input function behave like a basis -- for internal reasons,
     # this makes assembly easier.
     # TODO: Could be moved inside assemble_rhs
-    fnc = BasisFunctions.from_function(section_traction)
+    traction_function = BasisFunctions.from_function(section_traction)
     # Assemble the rhs, composed of the displacements induced by the traction
     # inputs.
-    rhs_assembler = RHSAssembler(mesh, bf, dh, qs)
-    rhs = rhs_assembler.assemble_rhs(fnc, k_d)
+    print("Assembling RHS")
+    rhs_assembler = RHSAssembler(mesh, bf, dh, qs_rhs)
+    rhs = rhs_assembler.assemble_rhs(traction_function, k_d)
 
     # Solve Ax = b, where x are the coefficients over the solution basis
     soln_coeffs = np.linalg.solve(Gup, rhs)
@@ -99,54 +108,72 @@ def main(n_elements, element_deg, plot):
     soln = Solution(bf, dh, soln_coeffs)
 
     # Evaluate that solution at 400 points around the circle
-    x, u = tools.evaluate_boundary_solution(400 / n_elements, soln, bf, mesh)
+    x, u = tools.evaluate_boundary_solution(400 / n_elements, soln, mesh)
 
     # Now, compute some interior values of stress along the x axis
+    print("Performing interior computations.")
     x_vals = np.linspace(0, 1.0, 11)[:-1]
 
+    # We use a slightly higher order quadrature for interior point
+    # computations just to avoid problems in testing. Could be reduced
+    # in the future.
+    interior_quadrature = QuadGauss(interior_quad_pts)
+    ip = InteriorPoint(mesh, dh, interior_quadrature)
 
-    # ip = InteriorPoint(mesh,
-    #                    bf, #BasisFunctions.from_function(section_traction),
-    #                    k_d, k_t,
-    #                    interior_k_d, interior_k_t, dh,
-    #                    QuadGauss(interior_quad_pts, 0.0, 1.0))
-    # ones_like_disp = np.ones_like(disp)
-    # int_strs = np.array(
-    #         [ip.compute_stress(
-    #             (x_v, 0.0), disp, trac) for x_v in x_vals])
-    # int_disp = np.array(
-    #         [ip.compute_displacement(
-    #             (x_v, 0.0), disp, trac) for x_v in x_vals])
+    # Get the tractions on the y-z plane (\sigma_xx, \sigma_xy)
+    normal = np.array([1.0, 0.0])
+    # Negative contribution of the AdjointTraction kernel
+    int_strs_x = -np.array(
+            [ip.compute((x_v, 0.0), normal, k_ta, traction_function)
+             for x_v in x_vals])
+    # Positive contribution of the hypersingular kernel
+    int_strs_x += np.array(
+            [ip.compute((x_v, 0.0), normal, k_h, soln)
+             for x_v in x_vals])
+
+    # # Get the tractions on the x-z plane (\sigma_xy, \sigma_yy)
+    # normal = np.array([0.0, 1.0])
+    # # Negative contribution of the AdjointTraction kernel
+    # int_strs_y = -np.array(
+    #         [ip.compute((x_v, 0.0), normal, k_ta, traction_function)
+    #          for x_v in x_vals])
+
+    # # Positive contribution of the hypersingular kernel
+    # int_strs_y += np.array(
+    #         [ip.compute((x_v, 0.0), normal, k_h, soln)
+    #          for x_v in x_vals])
 
     sigma_xx_exact = np.array([0.0398, 0.0382, 0.0339, 0.0278, 0.0209,
                       0.0144, 0.0089, 0.0047, 0.0019, 0.0004])
+
+    # Finally, plot some of the results.
     if plot:
+        # u_x
         plt.figure(2)
-        plt.plot(x[:, 0], s[:, 0])
+        plt.plot(x[:, 0], u[:, 0])
         plt.xlabel(r'X')
         plt.ylabel(r'$u_x$', fontsize = 18)
+
+        # u_y
         plt.figure(3)
-        plt.plot(x[:, 0], s[:, 1])
+        plt.plot(x[:, 0], u[:, 1])
         plt.xlabel(r'X')
         plt.ylabel(r'$u_y$', fontsize = 18)
-        # plt.figure(4)
-        # plt.plot(x_vals, int_strs[:, 0, 0], label = r'$\sigma_{xx}$')
-        # # plt.figure(5)
-        # plt.plot(x_vals, int_strs[:, 1, 1], label = r'$\sigma_{yy}$')
-        # plt.plot(x_vals, sigma_xx_exact, label = 'exact')
-        # plt.xlabel('distance along x axis from origin')
-        # plt.ylabel(r'$\sigma_{xx}$ and $\sigma_{yy}$')
-        # plt.figure(6)
-        # plt.plot(y_vals, int_disp[:, 0])
-        # plt.figure(7)
-        # plt.plot(y_vals, int_disp[:, 1])
-        # plt.show()
-    # return int_strs[:, 0, 0]
-    # See section 2.7 of starfield and crouch for the standard formulas to
-    # convert from plane strain to plane stress.
+
+        plt.figure(4)
+        # \sigma_xx
+        plt.plot(x_vals, int_strs_x[:, 0], label = r'$\sigma_{xx}$')
+        # \sigma_yy
+        # plt.plot(x_vals, int_strs_y[:, 1], label = r'$\sigma_{yy}$')
+        plt.plot(x_vals, sigma_xx_exact, label = 'exact')
+        plt.xlabel('distance along x axis from origin')
+        plt.ylabel(r'$\sigma_{xx}$ and $\sigma_{yy}$')
+        plt.legend()
+
+    return int_strs_x[:, 0]
 
 if __name__ == "__main__":
-    sigma_xx = main(50, 1, True)
+    sigma_xx = main(50, 0, True)
     plt.show()
     sigma_xx_exact = np.array([0.0398, 0.0382, 0.0339, 0.0278, 0.0209,
                       0.0144, 0.0089, 0.0047, 0.0019, 0.0004])
@@ -154,8 +181,8 @@ if __name__ == "__main__":
                        0.0141, 0.0086, 0.0044, 0.0016, 0.0000])
     sigma_xx_crouch_200 = np.array([0.0396, 0.0380, 0.0337, 0.0276, 0.0208,
                        0.0142, 0.0087, 0.0045, 0.0018, 0.0002])
-    print tools.rmse(sigma_xx, sigma_xx_exact)
-    print tools.rmse(sigma_xx_crouch_100, sigma_xx_exact)
-    print tools.rmse(sigma_xx_crouch_200, sigma_xx_exact)
+    print tools.L2_error(sigma_xx, sigma_xx_exact)
+    print tools.L2_error(sigma_xx_crouch_100, sigma_xx_exact)
+    print tools.L2_error(sigma_xx_crouch_200, sigma_xx_exact)
     plt.show()
 

codim1/test/test_integration.py
@@ -89,7 +89,7 @@ def test_M_integral_same_dof():
     q = quadrature.QuadGauss(2)
     bf = basis_funcs.BasisFunctions.from_degree(1, msh)
     kernel = lambda x, n: np.array([[1.0, 0], [0, 1.0]])
-    M_local = single_integral(msh, kernel, bf, q, 0, 0, 0)
+    M_local = single_integral(msh, kernel, bf, bf, q, 0, 0, 0)
     # integral of (1-x)^2 from 0 to 1
     np.testing.assert_almost_equal(M_local[0, 0], 1.0 / 3.0)
 
@@ -98,7 +98,7 @@ def test_M_integral_same_dof_with_jacobian():
     q = quadrature.QuadGauss(2)
     bf = basis_funcs.BasisFunctions.from_degree(1, msh)
     kernel = lambda x, n: np.array([[1.0, 0], [0, 1.0]])
-    M_local = single_integral(msh, kernel, bf, q, 0, 0, 0)
+    M_local = single_integral(msh, kernel, bf, bf, q, 0, 0, 0)
     # Element size divided by two so the M value should be divided by two
     np.testing.assert_almost_equal(M_local[0, 0], 1.0 / 6.0)
 
@@ -107,6 +107,6 @@ def test_M_integral_diff_dof():
     q = quadrature.QuadGauss(2)
     bf = basis_funcs.BasisFunctions.from_degree(1, msh)
     kernel = lambda x, n: np.array([[1.0, 0], [0, 1.0]])
-    M_local = single_integral(msh, kernel, bf, q, 0, 0, 1)
+    M_local = single_integral(msh, kernel, bf, bf, q, 0, 0, 1)
     # integral of (1-x)*x from 0 to 1
     np.testing.assert_almost_equal(M_local[0, 0], 1.0 / 6.0)

codim1/test/test_tools.py
@@ -1,6 +1,6 @@
 import numpy as np
 from codim1.core.tools import interpolate, evaluate_boundary_solution, \
-                evaluate_solution_on_element, plot_mesh, rmse
+                evaluate_solution_on_element, plot_mesh, L2_error
 from codim1.core.dof_handler import DiscontinuousDOFHandler
 from codim1.core.basis_funcs import BasisFunctions, Solution
 from codim1.core.mesh import Mesh
@@ -9,11 +9,11 @@ from codim1.core.mesh import Mesh
 #     m = Mesh.circular_mesh(50, 1.0)
 #     plot_mesh(m)
 
-def test_rmse():
+def test_L2error():
     x1 = np.array([1.0, 2.0])
     x2 = np.array([0.5, 0.5])
-    exact = 1.118033988
-    np.testing.assert_almost_equal(exact, rmse(x1, x2), 5)
+    exact = np.sqrt(0.25 + 2.25) / (np.sqrt(0.5))
+    np.testing.assert_almost_equal(exact, L2_error(x1, x2), 5)
 
 def test_interpolate():
     n_elements = 2