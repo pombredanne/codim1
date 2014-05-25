915da83d8a9a2897efad5d5977084271682c7421
Ben Thompson <t.ben.thompson@gmail.com>
2014-05-24 23:33:25 -0400

    GLL quadrature for interior points.

--- codim1/assembly/interior_point.py
+++ codim1/assembly/interior_point.py
@@ -1,45 +1,39 @@
 import numpy as np
 from codim1.fast_lib import single_integral, ConstantBasis
 
+
 def interior_pt_rhs(mesh, pt_normal, kernel, rhs_fnc):
+    """
+    Calculate the influence at an interior point from a boundary condition
+    """
     result = np.zeros(2)
     kernel.set_interior_data(pt_normal[0], pt_normal[1])
-    one = ConstantBasis(np.ones(2))
     for e_k in mesh:
-        quad_info =\
+        quad_info, interior_integrator = \
             e_k.qs.get_interior_quadrature(e_k, pt_normal[0])
 
-        integral = single_integral(e_k.mapping.eval,
-                                   kernel,
-                                   one,
-                                   rhs_fnc,
-                                   quad_info,
-                                   0, 0)
+        integral = interior_integrator(e_k.mapping.eval, kernel,
+                                       rhs_fnc, quad_info, 0)
         for idx1 in range(2):
             for idx2 in range(2):
                 result[idx1] += integral[idx1][idx2]
     return result
 
 def interior_pt_soln(mesh, pt_normal, kernel, coeffs):
+    """
+    Calculate the influence at an interior point from the solution.
+    """
     result = np.zeros(2)
     kernel.set_interior_data(pt_normal[0], pt_normal[1])
-    one = ConstantBasis(np.ones(2))
     for e_k in mesh:
-        quad_info =\
+        quad_info, interior_integrator = \
             e_k.qs.get_interior_quadrature(e_k, pt_normal[0])
 
         for i in range(e_k.basis.n_fncs):
             dof = e_k.dofs[:, i]
-            integral = single_integral(e_k.mapping.eval,
-                                       kernel,
-                                       one,
-                                       e_k.basis,
-                                       quad_info,
-                                       0, i)
+            integral = interior_integrator(e_k.mapping.eval, kernel,
+                                           e_k.basis, quad_info, i)
             for idx1 in range(2):
                 for idx2 in range(2):
                     result[idx1] += integral[idx1][idx2] * coeffs[dof[idx2]]
     return result
-
-def interior_pt_new_rhs(mesh, pt_normal, kernel, rhs_fnc):
-    pass
