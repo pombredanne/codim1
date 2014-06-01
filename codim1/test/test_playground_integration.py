7001e386159add0803e98ab4b2bb4aced4738fdc
Ben Thompson <t.ben.thompson@gmail.com>
2014-06-01 03:20:23 -0400

    Moved mathematica files to quadracheer. Working on integrating exactly!

--- codim1/test/test_playground_integration.py
+++ codim1/test/test_playground_integration.py
@@ -1,7 +1,8 @@
 import numpy as np
 from codim1.core import *
 from codim1.fast_lib import aligned_single_integral, TractionKernel,\
-                            single_integral, ConstantBasis
+                            single_integral, ConstantBasis, HypersingularKernel,\
+                            DisplacementKernel
 
 def test_basis_quad_aligned_ordering():
     bf = gll_basis(9)
@@ -36,9 +37,12 @@ def test_rl_integral():
     quad_info_old = lobatto(5)
     x_bf = np.array(bf.nodes)
     x_q = np.array(quad_info_old.x)
-    quad_info_new = rl_quad(5, 0.0, 5.0, 5)
+    distance = 5.0
+    quad_info_new = rl_quad(5, 0.0, distance, 5)
+    # This one is comparing lobatto quadrature and recursive legendre quadrature
+    # on the TractionKernel which is 1/r singular.
 
-    pt = [0.0, -5.0]
+    pt = [0.0, -distance]
     normal = [0.0, 1.0]
 
     # exact = 0.2473475767
@@ -56,8 +60,29 @@ def test_rl_integral():
     # This stuff doesn't work yet
     est_new = single_integral(mapping.eval,
                               kernel, one, bf, quad_info_new, 0, 0)
-    np.testing.assert_almost_equal(est_new[0][0], exact)
+    np.testing.assert_almost_equal(est_new[0][0], exact, 6)
 
-    # est_new_fast = aligned_single_integral(mapping.eval, kernel, bf,
-    #                                  quad_info_new, 0)
-    # np.testing.assert_almost_equal(est_new_fast[0][0], exact)
+    est_new_fast = aligned_single_integral(mapping.eval, kernel, bf,
+                                     quad_info_new, 0)
+    np.testing.assert_almost_equal(est_new_fast[0][0], exact, 6)
+
+def test_gauss_displacement_xy():
+    mesh = simple_line_mesh(1, (0, 0), (1, 0))
+    mapping = PolynomialMapping(mesh.elements[0])
+    kernel = DisplacementKernel(1.0, 0.25)
+    degree = 1
+    bf = gll_basis(degree)
+    one = ConstantBasis(np.ones(2))
+    quad_info_exact = gauss(100)
+    quad_info = lobatto(degree + 2)
+
+    # Testing GLL basis and quadrature combination on the nonsingular part
+    # of the displacement kernel.
+    pt = [0.5, 1000.0]
+    normal = [0.0, 1.0]
+    kernel.set_interior_data(pt, normal)
+
+    integrate = lambda qi: single_integral(mapping.eval, kernel, one, bf, qi, 0, 0)
+    exact = integrate(quad_info_exact)
+    est = integrate(quad_info)
+    np.testing.assert_almost_equal(exact[0][1], est[0][1], 16)
